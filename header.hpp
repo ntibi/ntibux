29ce39913b635cc417fb8303f926df3aa1c02e73
ntibi <ntibi@ntibi>
2017-10-09 20:29:03 -0400

    tweaked timer values

--- header.hpp
+++ header.hpp
@@ -21,9 +21,9 @@
 
 #define KEYBOARD_BUFFER_SIZE 256
 
-#define CLOCK_FREQ 1000                 // Hz             // TODO: benchmark avg process switch time and set CLOCK_FREQ accordingly
+#define CLOCK_FREQ 500                 // Hz             // TODO: benchmark avg process switch time and set CLOCK_FREQ accordingly
 #define MS_INTERVAL (1000 / CLOCK_FREQ) // interval in ms
-#define TIME_SLICE 50                   // in ticks
+#define TIME_SLICE 10                   // in ticks
 
 #define KERNEL_STACK_SIZE 16384
 
